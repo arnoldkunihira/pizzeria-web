3499887a344f900488e0d406262dd16d8897de79
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-08-10 16:17:39 +0300

    Import router and store before mounting the app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BootstrapIcon from '@dvuckovic/vue3-bootstrap-icons'
+import 'jquery'
+import Popper from 'popper.js'
+import '/node_modules/bootstrap'
+import router from './routes/web'
+import store from './store'
 
-createApp(App).mount('#app')
+Popper.Defaults.modifiers.computeStyle.gpuAcceleration = false;
+
+const app = createApp(App);
+
+app.use(router);
+
+app.use(store);
+
+app.component('BootstrapIcon', BootstrapIcon);
+
+app.mount('#app');
