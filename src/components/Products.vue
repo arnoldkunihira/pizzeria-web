ee612db1d8e497a65f4b52aa797bbec183ff84a4
Arnold Kunihira <arnold.kmulera@gmail.com>
2021-08-11 09:53:38 +0300

    Include header component into products page

---
+++ src/components/Products.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <div class="container">
+            <Header/>
+        </div>
+    </main>
+</template>
+
+<script setup>
+
+import { defineProps, reactive } from 'vue'
+import Header from '../components/partials/Header.vue'
+
+</script>
---
+++ src/components/partials/Header.vue
@@ -0,0 +1,16 @@
+<template>
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <img  src="/src/assets/pizza.png" alt="" width="32" height="32"> &nbsp;
+        <span class="fs-4">Pizza Hub</span>
+    </a>
+
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="/" class="nav-link" aria-current="page">Home</a></li>
+        <li class="nav-item "><a href="/login" class="nav-link b">Login</a></li>
+        <li class="nav-item "><a href="/register" class="nav-link b">Register</a></li>
+    </ul>
+    
+</header>
+</template>
